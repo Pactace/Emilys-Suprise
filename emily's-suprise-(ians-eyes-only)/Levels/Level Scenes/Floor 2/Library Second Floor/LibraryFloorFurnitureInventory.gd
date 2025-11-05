1d2d5181bc3a9d6e5da404baee9d7f9eee3ed359
Pactace <iantheartist01@gmail.com>
2025-11-05 15:39:07 -0700

    The house is layed out completely

---
+++ emily's-suprise-(ians-eyes-only)/Levels/Level Scenes/Floor 2/Library Second Floor/LibraryFloorFurnitureInventory.gd
@@ -0,0 +1,19 @@
+extends Resource
+class_name LibraryFurnitureInventory
+
+@export var objects := {
+	"res://Models/Final Models/Entryway/FtrElegantMirror/Textures/FtrElegantMirror_icon.png": preload("res://Models/Final Models/Entryway/FtrElegantMirror/FtrElegantMirror.tscn"),
+	"res://Models/Final Models/Entryway/FtrIronHanger/Textures/FtrIronHanger_icon.png": preload("res://Models/Final Models/Entryway/FtrIronHanger/FtrIronHanger.tscn"),
+	"res://Models/Final Models/Entryway/FtrAntiqueConsole/Textures/FtrAntiqueConsole_icon.png": preload("res://Models/Final Models/Entryway/FtrAntiqueConsole/FtrAntiqueConsole.tscn"),
+	"res://Models/Final Models/Entryway/FtrAntiqueTableS/Textures/FtrAntiqueTableS_icon.png": preload("res://Models/Final Models/Entryway/FtrAntiqueTableS/FtrAntiqueTableS.tscn"),
+	"res://Models/Final Models/Entryway/FtrIrongardenTable/Textures/FtrIrongardenTable_icon.png": preload("res://Models/Final Models/Entryway/FtrIrongardenTable/FtrIrongardenTable.tscn"),
+	"res://Models/Final Models/Entryway/FtrAlumiwoodtable/Textures/FtrAlumiwoodtable_icon.png": preload("res://Models/Final Models/Entryway/FtrAlumiwoodtable/FtrAlumiwoodtable.tscn"),
+	"res://Models/Final Models/Entryway/FtrMoroccanTableS/Textures/FtrMoroccanTableS_icon.png": preload("res://Models/Final Models/Entryway/FtrMoroccanTableS/FtrMoroccanTableS.tscn"),
+	"res://Models/Final Models/Entryway/FtrGardenTableSNatural/Textures/FtrGardenTableSNatural_icon.png": preload("res://Models/Final Models/Entryway/FtrGardenTableSNatural/FtrGardenTableSNatural.tscn"),
+	"res://Models/Final Models/Entryway/FtrCountryTableM/Textures/FtrCountryTableM_icon.png": preload("res://Models/Final Models/Entryway/FtrCountryTableM/FtrCountryTableM.tscn"),
+	"res://Models/Final Models/Entryway/FtrRattanTableS/Textures/FtrRattanTableS_icon.png": preload("res://Models/Final Models/Entryway/FtrRattanTableS/FtrRattanTableS.tscn"),
+	"res://Models/Final Models/Entryway/FtrTableSquareSimpleS/Textures/FtrTableSquareSimpleS_icon.png": preload("res://Models/Final Models/Entryway/FtrTableSquareSimpleS/FtrTableSquareSimpleS.tscn"),
+	"res://Models/Final Models/Entryway/FtrRoundTableClothS/Textures/FtrRoundTableClothS_icon.png": preload("res://Models/Final Models/Entryway/FtrRoundTableClothS/FtrRoundTableClothS.tscn"),
+	"res://Models/Final Models/Entryway/FtrWoodenChest/Textures/FtrWoodenChest_icon.png": preload("res://Models/Final Models/Entryway/FtrWoodenChest/FtrWoodenChest.tscn"),
+	"res://Models/Final Models/Entryway/FtrLowtableJapan/Textures/FtrLowtableJapan_icon.png": preload("res://Models/Final Models/Entryway/FtrLowtableJapan/FtrLowtableJapan.tscn"),
+}
---
+++ emily's-suprise-(ians-eyes-only)/Levels/Level Scenes/Floor 2/Library Second Floor/LibraryWallInventory.gd
@@ -0,0 +1,5 @@
+extends Resource
+class_name LibraryWallInventory
+
+@export var objects := {
+	}
